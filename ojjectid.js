e6ea8efa85ce5af281d6b912b39740d0639ab13e
Anuj Jain <jainanuj0812@gmail.com>
2018-05-09 18:51:28 +0530

    Added rental models and routes

---
+++ ojjectid.js
@@ -0,0 +1,9 @@
+// _id: 5af2e88ef10d88370ce9b0f0
+
+// 12 bytes
+    // 4 bytes: timestamp
+    // 3 bytes: machine identifier
+    // 2 bytes: process identifier
+    // 3 bytes: counter
+
+// drivers are responsible for generating ids, not mongoDB
